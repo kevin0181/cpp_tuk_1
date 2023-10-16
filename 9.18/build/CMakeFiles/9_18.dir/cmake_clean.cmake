209c4e75f42d2e2add6a40f041b689fada2cbce6
neneee <kevin0181@naver.com>
2023-10-16 21:47:33 +0900

    3

---
+++ 9.18/build/CMakeFiles/9_18.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "9_18"
+  "9_18.pdb"
+  "CMakeFiles/9_18.dir/main.cpp.o"
+  "CMakeFiles/9_18.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9.18/build/CMakeFiles/9_18.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 9_18.
+# This may be replaced when dependencies are built.
